25361f76a3154b342018333481ea256cd52bf175
Daniel <dafesata@gmail.com>
2017-05-06 10:30:59 -0500

    Spinner Ok!!

--- app/src/main/java/com/uninorte/proyecto1/Actividad_Evaluar.java
+++ app/src/main/java/com/uninorte/proyecto1/Actividad_Evaluar.java
@@ -1,5 +1,6 @@
 package com.uninorte.proyecto1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
