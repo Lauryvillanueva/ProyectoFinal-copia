8e409f76b51bb2d6f3b04136c4c6792b9368a022
LauryV <lauryv@uninorte.edu.co>
2017-05-09 16:19:24 -0500

    Plantilla de Login

--- app/src/main/java/com/uninorte/proyecto1/Welcome.java
+++ app/src/main/java/com/uninorte/proyecto1/Welcome.java
@@ -1,9 +1,19 @@
 package com.uninorte.proyecto1;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Welcome extends AppCompatActivity {
 
@@ -14,8 +24,50 @@ public class Welcome extends AppCompatActivity {
     }
 
     public void onClick_Iniciar(View view) {
-        Intent i = new Intent(Welcome.this,Home.class);
-        startActivityForResult(i,1);
+        showLoginDialog();
+    }
+
+    public void onClick_Registrarse(View view) {
 
     }
+
+    private void showLoginDialog()
+    {
+        LayoutInflater li = LayoutInflater.from(this);
+
+        View prompt = li.inflate(R.layout.login_dialog, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        alertDialogBuilder.setView(prompt);
+        final EditText user = (EditText) prompt.findViewById(R.id.login_name);
+        final EditText pass = (EditText) prompt.findViewById(R.id.login_password);
+
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id)
+                    {
+                     //**FIREBASES PARA LOGIN -----------------------------
+
+                        Intent i = new Intent(Welcome.this,Home.class);
+                        startActivityForResult(i,1);
+                        //----------------------------------------------
+                    }
+                });
+
+        alertDialogBuilder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id)
+            {
+                dialog.cancel();
+
+            }
+        });
+
+        alertDialogBuilder.show();
+
+    }
+
+
+
 }
