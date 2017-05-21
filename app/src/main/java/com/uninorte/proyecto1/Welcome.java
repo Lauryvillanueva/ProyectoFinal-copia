3c6505425a4969f6c7bb7627d2ecfd80ce16fef0
LauryV <lauryv@uninorte.edu.co>
2017-05-21 14:46:26 -0500

    Login Google

--- app/src/main/java/com/uninorte/proyecto1/Welcome.java
+++ app/src/main/java/com/uninorte/proyecto1/Welcome.java
@@ -1,6 +1,7 @@
 package com.uninorte.proyecto1;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
@@ -11,6 +12,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.LayoutInflater;
@@ -19,15 +21,25 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -36,15 +48,21 @@ import com.google.firebase.database.ValueEventListener;
 
 import fr.ganfra.materialspinner.MaterialSpinner;
 
-public class Welcome extends AppCompatActivity {
+public class Welcome extends AppCompatActivity  {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private String TAG=Welcome.class.getName();
     private ProgressDialog progressDialog;
     private Snackbar snackbar;
     private ConstraintLayout welcomeLayout;
-
     private DatabaseReference mDatabase;
+    private LinearLayout layout_or;
+
+    //google
+    SignInButton loginbtn;
+    private static final int RC_SIGN_IN = 0;
+    private GoogleApiClient mGoogleApiClient;
+
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,13 +84,115 @@ public class Welcome extends AppCompatActivity {
 
             }
         };
+
+        // Google
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener(){
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+
+                if (firebaseAuth.getCurrentUser()!=null){
+                    // Toast.makeText(getApplicationContext(), "usuario "+user.getPhotoUrl(),Toast.LENGTH_SHORT).show();
+                    /*User userG=new User("Vendedor",firebaseAuth.getCurrentUser().getEmail());
+                    FirebaseDatabase.getInstance().getReference().child("users").child(firebaseAuth.getCurrentUser().getUid()).setValue(userG);
+                    */Intent intent = new Intent(Welcome.this, Home.class);
+                    startActivity(intent);
+                }
+
+            }
+        };
+
+
+
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                        Toast.makeText(getApplicationContext(), connectionResult.getErrorMessage(), Toast.LENGTH_SHORT)
+                                .show();
+
+                    }
+                }).addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+
+        //
+    }
+
+
+
+    //google
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                // ...
+            }
+        }
     }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(Welcome.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        // ...
+                    }
+                });
+    }
+    //----------------------------------
+    //
+
+
 
     @Override
     public void onStop() {
@@ -105,6 +225,21 @@ public class Welcome extends AppCompatActivity {
         final EditText email = (EditText) prompt.findViewById(R.id.login_email);
         final EditText password = (EditText) prompt.findViewById(R.id.login_password);
         final TextView title=(TextView) prompt.findViewById(R.id.toolbar_title);
+        //google-------------------------
+        final LinearLayout linear_or = (LinearLayout) prompt.findViewById(R.id.login_layout_or);
+        final SignInButton logingoogle = (SignInButton) prompt.findViewById(R.id.loginbtn);
+        //loginbtn = (SignInButton) findViewById(R.id.loginbtn);
+        //--------------------------------------------------
+
+        logingoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                signIn();
+
+            }
+        });
+
         final MaterialSpinner spRole=(MaterialSpinner) prompt.findViewById(R.id.spinnerRole);
         final String[] role = new String[1];
         String[] valores = {"Estudiante","Profesor"};
@@ -133,7 +268,10 @@ public class Welcome extends AppCompatActivity {
             usernameWrapper.setHint("Username o Email");
             title.setText("Ingresar");
         }else{
+
             forgotpswd.setVisibility(View.GONE);
+            linear_or.setVisibility(View.GONE);
+            logingoogle.setVisibility(View.GONE);
             title.setText("Registro");
         }
 
@@ -142,12 +280,29 @@ public class Welcome extends AppCompatActivity {
                 .setPositiveButton("Ok", null) //Set to null. We override the onclick
                 .setNegativeButton("Cancelar", null);
 
+        //-------------------------------------------------------
+
         AlertDialog alertDialog=alertDialogBuilder.create();
+
+       /* //google
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                signIn();
+
+            }
+        });
+        //*/
 
 
         alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+
+
             @Override
             public void onShow(final DialogInterface dialog) {
+
+
                 Button button = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -268,6 +423,18 @@ public class Welcome extends AppCompatActivity {
                 });
     }
 
+    public void onClick_IniciarGoogle(View view) {
+        signIn();
+    }
 
 
+   /* @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.login_btn_forgot_password)
+        {
+          //  startActivity(new Intent(Welcome.this,ForgotPassword.class));
+            finish();
+
+        }
+    }*/
 }
